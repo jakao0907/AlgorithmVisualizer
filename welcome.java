29ae96d0162ba3e7c9e988cb7f4a4ca225c5734a
jakao0907 <47694391+jakao0907@users.noreply.github.com>
2020-05-11 22:42:38 +0800

    welcome.java add three button test success

--- welcome.java
+++ welcome.java
@@ -6,8 +6,15 @@ import javax.swing.*;
 
 public class welcome{
     public static void main(String args[]){
-        JFrame mainWindow = new JFrame();
-        mainWindow.setSize(400,300);
+    	String[] algorithm = {"Minimum Spanning Tree","Shortest Path","Convex Hull"};
+        JFrame mainWindow = new JFrame("演算法視覺化");
+        mainWindow.setSize(1000,750);
+        mainWindow.getContentPane().setLayout(new FlowLayout());
+
+        for(int i=0;i<algorithm.length;i++){
+        	JButton btn = new JButton(algorithm[i]);
+        	mainWindow.add(btn);
+        }
 
         mainWindow.setVisible(true);
     }
