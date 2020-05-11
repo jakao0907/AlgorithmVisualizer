dcea6d0eb826bb75a699115e022a1e88a8cf65f6
jakao0907 <47694391+jakao0907@users.noreply.github.com>
2020-05-11 22:04:54 +0800

    welcome.java update

--- welcome.java
+++ welcome.java
@@ -1,8 +1,14 @@
 package ntou.cs.java2020.project;
 
-import 
+import java.util.*;
+import java.awt.*;
+import javax.swing.*;
+
 public class welcome{
     public static void main(String args[]){
-        System.out.print("HI");
+        JFrame mainWindow = new JFrame();
+        mainWindow.setSize(400,300);
+
+        mainWindow.setVisible(true);
     }
 }
