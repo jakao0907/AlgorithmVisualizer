1beeda845d7dbb67340d74a195774fd8e619c0bd
jakao0907 <47694391+jakao0907@users.noreply.github.com>
2020-06-02 23:09:41 +0800

    update minimumSpanningTreeGUI完成接收輸入,下一步為完成圖像畫呈現邊

--- minimumSpanningTreeGUI.java
+++ minimumSpanningTreeGUI.java
@@ -1,5 +1,7 @@
 package ntou.cs.java2020.project;
 
+import java.util.*;
+import java.io.*;
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -7,46 +9,73 @@ import java.awt.event.*;
 public class minimumSpanningTreeGUI extends JFrame implements ActionListener{
 
 	private final int guiWidth=500,guiHeight=500;
-	JLabel vertexNumberLabel,edgeNumberLabel,errLabel;
-	JButton buildButton,startButton;
-	JTextField vertexNumberText,edgeNumberTest;
-	JPanel functionPanel,errPanel;
+	protected JLabel vertexNumberLabel,errLabel,fromPointLabel,toPointLabel,weightPointLabel;
+	protected JButton buildButton,startButton,addEdgeButton;
+	protected JTextField vertexNumberText,weightPointTest;
+	protected JPanel functionPanel,errPanel,addEdgePanel;
+	protected JComboBox fromPointTest,toPointTest;
+	// private ArrayList<edge> G;
 
 	public minimumSpanningTreeGUI(){
 
 		vertexNumberLabel = new JLabel("輸入點的數量");
 		vertexNumberText = new JTextField(10);
 
-		edgeNumberLabel	= new JLabel("輸入邊的數量(需連通所有點)");
-		edgeNumberTest = new JTextField(10);
-
-		buildButton = new JButton("建圖");
+		buildButton = new JButton("確定");
 		startButton = new JButton("開始動畫");
 		startButton.setEnabled(false);
 
 		errLabel = new JLabel(" ");
 		errLabel.setForeground(Color.red);
-
-		functionPanel = new JPanel(new FlowLayout());
+		//first line input vertex number and start teacher function
+		functionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
 		functionPanel.add(vertexNumberLabel);
 		functionPanel.add(vertexNumberText);
-		functionPanel.add(edgeNumberLabel);
-		functionPanel.add(edgeNumberTest);
 		functionPanel.add(buildButton);
 		functionPanel.add(startButton);
-		add(functionPanel,BorderLayout.NORTH);
+		add(functionPanel);
+
+		//secode line addedge function
+		fromPointLabel = new JLabel("加邊-> 端點1:");
+		fromPointTest = new JComboBox();
+		fromPointTest.setEnabled(false);
+
+		toPointLabel = new JLabel("端點2:");
+		toPointTest = new JComboBox();
+		toPointTest.setEnabled(false);
+
+		weightPointLabel = new JLabel("權重為:");
+		weightPointTest = new JTextField(10);
+		weightPointTest.setEnabled(false);
+
+		addEdgeButton = new JButton("新增");
+		addEdgeButton.setEnabled(false);
+
+		addEdgePanel = new JPanel(new FlowLayout());
+
+		addEdgePanel.add(fromPointLabel);
+		addEdgePanel.add(fromPointTest);
+		addEdgePanel.add(toPointLabel);
+		addEdgePanel.add(toPointTest);
+		addEdgePanel.add(weightPointLabel);
+		addEdgePanel.add(weightPointTest);
+		addEdgePanel.add(addEdgeButton);
+		add(addEdgePanel);
 
 		errPanel = new JPanel();
 		errPanel.add(errLabel);
 		add(errPanel,BorderLayout.SOUTH);
 
+		fromPointTest.addActionListener(this);
 		buildButton.addActionListener(this);
 		startButton.addActionListener(this);
+		addEdgeButton.addActionListener(this);
 
-		setSize(800,500);
+		setSize(550,500);
 		setTitle("minimum Spanning Tree");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLayout(new FlowLayout());
 		setVisible(true);
 	}
 
@@ -54,20 +83,69 @@ public class minimumSpanningTreeGUI extends JFrame implements ActionListener{
 		new minimumSpanningTreeGUI();
 	}
 
+	public void paintBasicPoint(){
+		ArrayList<pair> pointPosition = new ArrayList<pair>();
+
+	}
+
 	public void actionPerformed(ActionEvent e) {
-		if(e.getSource()==buildButton){
+		if(e.getSource()==fromPointTest){
 			int n=Integer.parseInt(vertexNumberText.getText());
-			int m=Integer.parseInt(edgeNumberTest.getText());
-			if(n-m>1){
-				errLabel.setText("邊的數量過少!");
+			if(fromPointTest.getSelectedItem()!="請選擇"){
+				toPointTest.removeAllItems();
+				toPointTest.addItem("請選擇");
+				for(int i=1;i<=n;i++){
+					if(i!=Integer.parseInt(fromPointTest.getSelectedItem().toString()))
+					toPointTest.addItem(i);
+				}
+				toPointTest.setEnabled(true);
+			}
+		}
+		else if(e.getSource()==buildButton){
+			if(vertexNumberText.getText().equals("")){
+				errLabel.setText("無輸入!!!");
 			}
 			else{
+				paintBasicPoint();
 				errLabel.setText("");
+				int n = Integer.parseInt(vertexNumberText.getText());
+				fromPointTest.removeAllItems();
+				fromPointTest.addItem("請選擇");
+				for(int i=1;i<=n;i++){
+					fromPointTest.addItem(i);
+				}
+				fromPointTest.setEnabled(true);
+				weightPointTest.setEnabled(true);
+				addEdgeButton.setEnabled(true);
 				startButton.setEnabled(true);
 			}
 		}
 		else if(e.getSource()==startButton){
-			System.out.println("!!");
+		}
+		else if(e.getSource()==addEdgeButton){
+			if(weightPointTest.getText().equals("")){
+				errLabel.setText("請輸入權重!");
+			}
+			else if(fromPointTest.getSelectedItem().equals("請選擇")){
+				errLabel.setText("請選擇端點1");
+			}
+			else if(toPointTest.getSelectedItem().equals("請選擇")){
+				errLabel.setText("請選擇端點2");
+			}
+			else if(Integer.parseInt(weightPointTest.getText())<=0){
+				errLabel.setText("邊權重需為正整數!");
+			}
+			else{
+				errLabel.setText("");
+				int a = Integer.parseInt(fromPointTest.getSelectedItem().toString());
+				int b = Integer.parseInt(toPointTest.getSelectedItem().toString());
+				int c = Integer.parseInt(weightPointTest.getText());
+				// G.add(edge(a,b,c));
+			}
+		}
+		else{
+			System.out.println("?");
 		}
 	}
+
 }
--- minimumSpanningTreeTest.java
+++ minimumSpanningTreeTest.java
@@ -1,19 +1,29 @@
 package ntou.cs.java2020.project;
 
 import java.util.*;
+import java.io.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
 
-public class minimumSpanningTreeTest{
+public class minimumSpanningTreeTest extends minimumSpanningTreeGUI{
 	private static int n=3;
 	private static int m=2;
-	private static ArrayList<edge> testGraph = new ArrayList<edge>();
-
+	protected static ArrayList<edge> testGraph = new ArrayList<edge>();
+	private static minimumSpanningTreeGUI gui;
 	public static void main(String[] args){
-		testGraph.add(new edge(1,2,10));
-		testGraph.add(new edge(2,3,4));
+		gui = new minimumSpanningTreeGUI();
+		// testGraph.add(new edge(1,2,10));
+		// testGraph.add(new edge(2,3,4));
 		minimumSpanningTree testG = new minimumSpanningTree(n,m,testGraph);
 		ArrayList<edge> minimumSpanningTreeOrder = testG.solve();
 		for(edge x:minimumSpanningTreeOrder){
 			System.out.println(x);
+		}
+	}
+	public void actionPerformed(ActionEvent e){
+		if(e.getSource()==super.startButton){
+			System.out.print("success");
 		}
 	}
 }
